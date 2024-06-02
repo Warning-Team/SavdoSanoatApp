594ab0f6a1eb92be43d938ec9fccd8097de163c7
Bexruzbek-dev <behruz.toshpolatov14@gmail.com>
2024-06-02 12:01:21 +0500

    checking 2

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed azamatjon',
+              'You have pushed Behruz',
             ),
             Text(
               '$_counter',
