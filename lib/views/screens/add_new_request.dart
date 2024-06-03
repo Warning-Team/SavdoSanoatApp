9ddaa990add55e2b16f3aec90c3bf35e03989166
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-06-03 13:59:17 +0500

    login

---
+++ lib/views/screens/add_new_request.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddNewRequest extends StatefulWidget {
+  const AddNewRequest({super.key});
+
+  @override
+  State<AddNewRequest> createState() => _AddNewRequestState();
+}
+
+class _AddNewRequestState extends State<AddNewRequest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:savdosanoatapp/views/screens/add_new_request.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>  AddNewRequest(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "profile",
+          )
+        ],
+      ),
+    );
   }
 }
