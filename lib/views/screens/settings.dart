0ec0d7a90dd2e412fcb9e74a17bc180fac2c1f41
parvisss <ismoilzhonovparviz@gmail.com>
2024-06-30 12:00:43 +0500

    a

--- lib/views/screens/settings.dart
+++ lib/views/screens/settings.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Settings extends StatelessWidget {
-  const Settings({super.key});
-
+  Settings({super.key});
+  List options = [
+    "change password",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: options.length,
+                itemBuilder: (ctx, index) {
+                  return InkWell(
+                    child: ListTile(
+                      title: Text(
+                        options[index],
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_forward_ios_sharp,
+                          size: 15,
+                        ),
+                      ),
+                    ),
+                    onTap: () {},
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
