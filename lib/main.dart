e0df0b674def733b249d38ffb4d111e36cdda2a7
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-06-30 11:06:31 +0500

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
           debugShowCheckedModeBanner: false,
-          home: Settings(),
+          home: OnboardingScreen(),
         );
       }),
     );
