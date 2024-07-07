7110dc3f482f5584e46d12c6f4363a81ea8aac86
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-07-07 13:52:24 +0500

    wdf

---
+++ lib/views/screens/request_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RequestScreen extends StatefulWidget {
+  const RequestScreen({super.key});
+
+  @override
+  State<RequestScreen> createState() => _RequestScreenState();
+}
+
+class _RequestScreenState extends State<RequestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Request"),
+        centerTitle: true,
+      ),
+      
+    );
+  }
+}
