2d53af9a1ff30a56722d140283d38191d677051b
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-07-14 14:59:25 +0500

    sd

--- lib/views/screens/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart' as ath;
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
